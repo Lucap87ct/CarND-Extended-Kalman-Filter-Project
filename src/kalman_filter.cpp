9b39baf75dcf92d291ce390ec3b07c276103e12e
Luca.Profumo <luca.profumo@fcagroup.com>
2020-01-22 12:13:59 +0100

    Implementation of Kalman filter and EKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,8 +3,8 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
+/*
+ * Please note that the Eigen library does not initialize
  *   VectorXd or MatrixXd objects with zeros upon creation.
  */
 
@@ -12,30 +12,73 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
-}
-
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+
+  // calculate predicted output z_pred from output matrix H and state x
+  VectorXd z_pred = H_ * x_;
+
+  // calculate y error between measurement z and predicted output z_pred
+  VectorXd y = z - z_pred;
+
+  UpdateStateAndCovarianceMatrix(y);
+}
+
+void KalmanFilter::UpdateStateAndCovarianceMatrix(const Eigen::VectorXd &y) {
+
+  // calculate Kalman gain K from output matrix H, state covariance matrix P and
+  // measurement noise matrix R
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // update state estimate x using Kalman gain K and y error
+  x_ = x_ + (K * y);
+
+  // update state covariance matrix P from Kalman gain K and measurement
+  // matrix H
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  VectorXd z_pred(3);
+
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // calculate components of z_pred from the state and the non linear
+  // measurement function h
+  float rho = sqrt(px * px + py * py);
+  // correction if calculated rho is too small
+  rho = max(rho, 0.001F);
+
+  float theta = atan2(py, px);
+  float rho_dot = (px * vx + py * vy) / rho;
+
+  z_pred << rho, theta, rho_dot;
+
+  // calculate y error between measurement z and predicted output z_pred
+  VectorXd y = z - z_pred;
+
+  // set calculated angle component of y error between -PI and +PI
+  float rho_corrected = y(1);
+  while (rho_corrected > M_PI) {
+    rho_corrected -= M_PI;
+  }
+  while (rho_corrected < -M_PI) {
+    rho_corrected += M_PI;
+  }
+  y(1) = rho_corrected;
+
+  UpdateStateAndCovarianceMatrix(y);
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -3,8 +3,10 @@
 
 #include "Eigen/Dense"
 
+using namespace std;
+
 class KalmanFilter {
- public:
+public:
   /**
    * Constructor
    */
@@ -14,18 +16,6 @@ class KalmanFilter {
    * Destructor
    */
   virtual ~KalmanFilter();
-
-  /**
-   * Init Initializes Kalman filter
-   * @param x_in Initial state
-   * @param P_in Initial state covariance
-   * @param F_in Transition matrix
-   * @param H_in Measurement matrix
-   * @param R_in Measurement covariance matrix
-   * @param Q_in Process covariance matrix
-   */
-  void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-            Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
 
   /**
    * Prediction Predicts the state and the state covariance
@@ -45,6 +35,12 @@ class KalmanFilter {
    * @param z The measurement at k+1
    */
   void UpdateEKF(const Eigen::VectorXd &z);
+
+  /**
+   * Updates the state by using Extended Kalman Filter equations
+   * @param y The error at k+1
+   */
+  void UpdateStateAndCovarianceMatrix(const Eigen::VectorXd &y);
 
   // state vector
   Eigen::VectorXd x_;
